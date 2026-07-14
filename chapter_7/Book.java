e6062cede88a6fa5f6a0e455a252c5f888fa5451
pc_lab_info_11 <cledson.araujo@escolar.ifrn.edu.br>
2026-07-14 19:33:48 -0300

    Remodificando os atributos e criando o método para empréstimo do livro

--- chapter_7/Book.java
+++ chapter_7/Book.java
@@ -1,20 +1,30 @@
 import java.util.ArrayList;
 
 public class Book {
-    private ArrayList<String> title;
-    private ArrayList<String> author;
-    private ArrayList<String> isbn;
+    private String title;
+    private String author;
+    private String isbn;
     
-    private ArrayList<Boolean> available;
-    private ArrayList<Integer> borrowCount;
-    private ArrayList<Double> rating;
+    private boolean available;
+    private int borrowCount;
+    private double rating;
 
     
-    public Book(ArrayList<String> title, ArrayList<String> author, ArrayList<String> isbn, ArrayList<Boolean> available) {
-        this.title.addAll(title);
-        this.author.addAll(author);
-        this.isbn.addAll(isbn);
-        this.available.add(false);
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+
+        this.available = true;
+    }
+
+    public void borrow() {
+        if (available) {
+            this.available = false;
+            System.out.println("Livro emprestado. Boa leitura!");
+        } else {
+            System.out.println("Lamento, livro já está em empréstimo.");
+        }
     }
 
 }
