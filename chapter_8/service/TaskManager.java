bd6b792ffe96c8c715fbace23dbc82ed2b5370f4
0xB4PH0 <rcleito129@gmail.com>
2026-07-21 18:52:37 -0300

    Desenvolvendo todos os métodos e suas devidas funcionalidades

--- chapter_8/service/TaskManager.java
+++ chapter_8/service/TaskManager.java
@@ -1,12 +1,181 @@
 package chapter_8.service;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import chapter_8.model.Task;
 
 public class TaskManager {
-    private Task[] tasks;
-    private int taskCount;
+    private ArrayList<Task> tasks;
     private ArrayList<String> history;
+    private Scanner consoleInput;
+    private int[][] productivity;
 
-    private 
+    public TaskManager() {
+
+        this.tasks = new ArrayList<Task>();
+        this.consoleInput = new Scanner(System.in);
+        this.productivity = new int[12][31];
+
+    }
+
+    public void addTask() {
+        System.out.print("Title: ");
+        String title = this.consoleInput.nextLine();
+
+        System.out.print("Description: ");
+        String description = this.consoleInput.nextLine();
+
+        System.out.println("1 -> High");
+        System.out.println("2 -> Medium");
+        System.out.println("3 -> Low");
+        System.out.print("Priority: ");
+        int priority = this.consoleInput.nextInt();
+        this.consoleInput.nextLine();
+
+        Task task = new Task(title, description, priority);
+        tasks.add(task);
+        System.out.println("Task added");
+    }
+
+    public boolean removeTask() {
+        this.consoleInput.nextLine();
+        System.out.print("Title: ");
+        String title = this.consoleInput.nextLine();
+        boolean status = false;
+
+        for (Task task : this.tasks) {
+            if (task.getTitle().equals(title)) {
+                tasks.remove(task);
+                status = true;
+                
+            }
+        }
+        return status;
+    }
+
+    public boolean searchTask() {
+        this.consoleInput.nextLine();
+        System.out.print("Title: ");
+        String title = this.consoleInput.nextLine();
+        boolean status = false;
+
+        for (Task task : this.tasks) {
+            if (task.getTitle().equals(title)) {
+                task.show();
+                status = true;
+            }
+        }
+        return status;
+    }
+
+    public boolean completeTask() {
+        System.out.print("Title: ");
+        String title = this.consoleInput.nextLine();
+
+        System.out.print("Concluded task month: ");
+        int month = this.consoleInput.nextInt();
+        System.out.print("Concluded task day: ");
+        int day = this.consoleInput.nextInt();
+        this.productivity[month - 1][day - 1]++;
+
+        boolean status = false;
+
+        for (Task task : this.tasks) {
+            if (task.getTitle().equals(title)) {
+                task.complete();
+                status = true;
+            }
+        }
+        return status;
+    }
+
+    public boolean reopenTask() {
+        this.consoleInput.nextLine();
+        System.out.print("Title: ");
+        String title = this.consoleInput.nextLine();
+        boolean status = false;
+
+        for (Task task : this.tasks) {
+            if (task.getTitle().equalsIgnoreCase(title)) {
+                task.reopen();
+                status = true;
+            }
+        }
+        return status;
+    }
+
+    public void showTasks() {
+        for (Task task : this.tasks) {
+            task.show();
+        }
+    }
+
+    public void showStatistics() {
+        int concludedTasksSum = 0;
+        for (Task task : this.tasks) {
+            if (task.isCompleted()) {
+                concludedTasksSum += 1;
+            }
+        }
+
+        int pendingTasksSum = 0;
+        for (Task task : this.tasks) {
+            if (task.isCompleted() == false) {
+                pendingTasksSum += 1;
+            }
+        }
+
+        double concludedTasksPercent = (double) concludedTasksSum / this.tasks.getFirst().getTotalTasksCreated();
+        
+        int[] prioritySums = new int[3];
+        String[] priorityNames = {"High", "Medium", "Low"};
+        int maxValueIndex = 0;
+
+        for (Task task : this.tasks) {
+            if (task.getPriority() == 1) {
+                prioritySums[0]++;
+            
+            } else if (task.getPriority() == 2) {
+                prioritySums[1]++;
+                
+            } else if (task.getPriority() == 3) {
+                prioritySums[2]++;
+            }
+        }
+
+        for (int count = 1; count < prioritySums.length; count++) {
+            if (prioritySums[count] > prioritySums[maxValueIndex]) {
+                maxValueIndex = count;
+            }
+        }
+
+        ArrayList<Task> highPriorityTasks = new ArrayList<Task>();
+        ArrayList<Task> lowPriorityTasks = new ArrayList<Task>();
+        for (Task task : this.tasks) {
+            if (task.getPriority() == 1) {
+                highPriorityTasks.add(task);
+            } else if (task.getPriority() == 3) {
+                lowPriorityTasks.add(task);
+            }
+        }
+
+        System.out.println("=========================================================");
+        System.out.printf("Total de tarefas: %d\n", this.tasks.getFirst().getTotalTasksCreated());
+        System.out.printf("Total de tarefas concluídas: %d\n", concludedTasksSum);
+        System.out.printf("Total de tarefas pendentes: %d\n", pendingTasksSum);
+        System.out.printf("Tarefas concluídas (percentual): %%%.2f\n", concludedTasksPercent * 100);
+        System.out.printf("Prioridade mais utilizada: %s\n", priorityNames[maxValueIndex]);
+        System.out.print("Tarefas de alta prioridade: \n");
+        highPriorityTasks.forEach(task -> task.show());
+        System.out.print("Tarefas de menor prioridade: \n");
+        lowPriorityTasks.forEach(task -> task.show());
+    }
+
+    public void showProductivityMatrix() {
+        for (int i = 0; i < this.productivity.length; i++) {
+            for (int j = 0; j < this.productivity[i].length; j++) {
+                System.out.print(this.productivity[i][j] + " ");
+            }
+        }
+    }
 }
