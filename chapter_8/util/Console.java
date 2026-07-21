989269ec4f281899c2905eec400bb43f755c18e9
0xB4PH0 <rcleito129@gmail.com>
2026-07-21 19:31:45 -0300

    Menu criado

--- chapter_8/util/Console.java
+++ chapter_8/util/Console.java
@@ -1,5 +1,19 @@
 package chapter_8.util;
 
 public class Console {
-    
+    public static void console() {
+        System.out.println("===================================================");
+        System.out.println("TASKFLOW");
+        System.out.println("1 - Add task");
+        System.out.println("2 - Edit task");
+        System.out.println("3 - Complete task");
+        System.out.println("4 - Reopen task");
+        System.out.println("5 - Remove task");
+        System.out.println("6 - Search task");
+        System.out.println("7 - List tasks");
+        System.out.println("8 - Statistics");
+        System.out.println("9 - History");
+        System.out.println("0 - Exit");
+        System.out.println("===================================================");
+    }
 }
