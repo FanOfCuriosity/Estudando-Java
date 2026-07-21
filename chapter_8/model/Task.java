81da9433813eb618cbff9d5b28906931b761f820
0xB4PH0 <rcleito129@gmail.com>
2026-07-21 00:48:35 -0300

    criação dos atributos da classe Task

--- chapter_8/model/Task.java
+++ chapter_8/model/Task.java
@@ -1,5 +1,11 @@
 package chapter_8.model;
 
 public class Task {
-    
+    private String title;
+    private String description;
+    private int priority;
+    private boolean completed;
+    private final String creationDate;
+    private String completionDate;
+    private static int totalTasksCreated;
 }
