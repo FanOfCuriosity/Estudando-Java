b167b5df34fa20361b5b3ce4c4e3d2f36fbe9f1b
0xB4PH0 <rcleito129@gmail.com>
2026-07-21 14:10:09 -0300

    Construindo o constructor da classe Task + pós-condições

--- chapter_8/model/Task.java
+++ chapter_8/model/Task.java
@@ -8,4 +8,25 @@ public class Task {
     private final String creationDate;
     private String completionDate;
     private static int totalTasksCreated;
+
+    public Task(String title, String description, int priority) {
+        if (title.isEmpty()) {
+            System.out.println("ERRO: Título vazio");
+
+        } else if (description.isEmpty()) {
+            System.out.println("ERRO: Descrição vazia");
+
+        } else if (priority < 1 || priority > 3) {
+            System.out.println("ERRO: Opção de prioriedade inexistente");
+        
+        } else {
+            this.title = title;
+            this.description = description;
+            this.priority = priority;
+            this.completed = false;
+            this.creationDate = java.time.LocalDate.now().toString();
+            totalTasksCreated++;
+        }
+
+    }
 }
